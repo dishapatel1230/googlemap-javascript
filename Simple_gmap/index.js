bcab8ea304bdf3a8eea178d0fe8574c4bb93509d
dishapatel1230 <dishaptel3012>
2022-09-28 16:40:35 +0530

    Simple_gmap

---
+++ Simple_gmap/index.js
@@ -0,0 +1,5 @@
+function myMap(){
+    var map_parameters = { center: {lat: 40.7648, lng: -73.9808}, zoom: 8 };
+    var map = new google.maps.Map(document.getElementById('map'), map_parameters);
+    }
+    
