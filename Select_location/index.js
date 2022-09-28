b08a75ac76cd2cd6d122849a91e9b6b686998b01
dishapatel1230 <dishaptel3012>
2022-09-28 16:39:43 +0530

    Select_location

---
+++ Select_location/index.js
@@ -0,0 +1,54 @@
+//This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
+
+
+let map , infoWindow;
+function myMap(){
+ map = new google.maps.Map(document.getElementById('map'),{
+       center:{lat:-34.397, lng: 150.644},
+       zoom :6,
+    });
+
+ //Overlays are objects on the map that are bound to latitude/longitude coordinates.
+ //Info Windows - Displays content within a popup balloon on top of a map
+ infoWindow = new google.maps.InfoWindow();
+ const locationButton = document.createElement("button");
+
+ locationButton.textContent ="Pan to Current location";
+ locationButton.classList.add("custom-map-control-button");
+ map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+ locationButton.addEventListener("click",() => {
+     if(navigator.geolocation){
+         navigator.geolocation.getCurrentPosition(
+             (position) => {
+                 const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                 };
+                 infoWindow.setPosition(pos);
+                 infoWindow.setContent("location.found");
+                 infoWindow.open(map);
+                 map.setCenter(pos);
+             },
+             () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+              }
+         );
+     } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+ });
+}
+function handleLocationError(browserHasGeolocation ,infoWindow,pos){
+    infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
